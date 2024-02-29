ac0d8599731042d27519a44e10cea028d790ab62
Ubbie <ubbiehawk@gmail.com>
2024-02-28 21:49:26 -0500

    Updated HomePage

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,19 +1,146 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late List<CategoryList> _chartData;
+  late TooltipBehavior _tooltipBehavior;
+  late TextEditingController _textFieldController = TextEditingController();
+  late SelectionBehavior _selectionBehavior;
+
+  @override
+  void initState() {
+    _chartData = getInitialChartData();
+    _tooltipBehavior = TooltipBehavior(enable: true);
+    _selectionBehavior = SelectionBehavior(enable: true);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              child: SfCircularChart(
+                //Clicking on Data Labels will send you to a new page
+                onDataLabelTapped: (onTapArgs) {
+                                    String tappedCategory = _chartData[onTapArgs.pointIndex].category;
+                   Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CategoryDetailPage(category: tappedCategory),
+                      ),
+                    );
+                },
+                //Pie Chart Creation
+                title: ChartTitle(text: 'Expense Tracker'),
+                legend: Legend(isVisible: true, overflowMode: LegendItemOverflowMode.wrap,
+                ),
+                tooltipBehavior: _tooltipBehavior,
+                selectionGesture: ActivationMode.singleTap,
+                series: <CircularSeries>[
+                  PieSeries<CategoryList, String>(
+                    dataSource: _chartData,
+                    xValueMapper: (CategoryList data, _) => data.category,
+                    yValueMapper: (CategoryList data, _) => data.percentage,
+                    enableTooltip: true,
+                    //Sets Data Labels as the Category Names
+                    dataLabelMapper: (CategoryList data, _) => data.category,
+                    dataLabelSettings: DataLabelSettings(isVisible: true),
+  
+                  //  selectionBehavior: _selectionBehavior,
+                  )
+                ],
+              ),
+            ),
+            ElevatedButton.icon(
+              onPressed: () {
+                addCategory();
+              },
+              icon: Icon(Icons.add),
+              label: Text('Add Category'),
+            ),
+            TextField(
+              controller: _textFieldController,
+              decoration: InputDecoration(labelText: "New Category"),
+            ),
+          ],
         ),
+      ),
+    );
+  }
+
+//Inital Categorys *IGNORE NUMBERS***
+  List<CategoryList> getInitialChartData() {
+    final List<CategoryList> chartData = [
+      CategoryList('Home', 11600),
+      CategoryList('Auto', 12490),
+      CategoryList('Grocery', 12900),
+      CategoryList('Savings', 23050),
+      CategoryList('Entertainment', 24880),
+      CategoryList('Bills', 34390),
+    ];
+    return chartData;
+  }
+
+//Adds a Categgory to the chart if Text-Field isn't Empty
+  void addCategory() {
+    setState(() {
+      String newCategory = _textFieldController.text;
+      if (newCategory.isNotEmpty) {
+        _chartData.add(CategoryList(newCategory, 12500));
+        _textFieldController.clear();
+      }
+    });
+  }
+}
+
+class CategoryList {
+  CategoryList(this.category, this.percentage);
+  final String category;
+  final int percentage;
+}
+
+class CategoryDetailPage extends StatelessWidget {
+  final String category;
+
+  const CategoryDetailPage({Key? key, required this.category}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Category Detail: $category'),
+      ),
+      body: Center(
+        child: Text('Details for category: $category'),
       ),
     );
   }
